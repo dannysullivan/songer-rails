138a7aa443772cc4dd53d95930702b2479601394
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-03-16 16:54:59 -0400

    Add bass notes to midi output

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,14 @@
 class Song < ActiveRecord::Base
   validates_presence_of :pattern
 
+  def bass_notes
+    [0, 5]
+  end
+
+  def measures
+    self.pattern.length / 8
+  end
+
   def to_midi
     midi_song = MIDI::Sequence.new
     meta_track = MIDI::Track.new(midi_song)
@@ -8,7 +16,9 @@ class Song < ActiveRecord::Base
     meta_track.events << MIDI::ProgramChange.new(0, 0)
 
     melody = MIDI::Track.new(midi_song)
+    bass = MIDI::Track.new(midi_song)
     midi_song.tracks << melody
+    midi_song.tracks << bass
     eighth_note_length = midi_song.note_to_delta('eighth')
     offset = 0
 
@@ -20,6 +30,12 @@ class Song < ActiveRecord::Base
       else
         offset += eighth_note_length
       end
+    end
+
+    bass_notes.cycle(self.measures/2) do |bass_interval|
+      note = 40 + bass_interval
+      bass.events << MIDI::NoteOn.new(0, note, 127, 0)
+      bass.events << MIDI::NoteOff.new(0, note, 127, eighth_note_length*8)
     end
 
     file = File.new('midifile.mid', 'wb')
