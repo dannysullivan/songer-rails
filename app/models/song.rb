7e99b9ccab76eff5b9d37b478061988f6dada423
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-03-18 00:08:58 -0400

    Add delay to beginning of midi playback so first note will play

--- app/models/song.rb
+++ app/models/song.rb
@@ -73,6 +73,12 @@ class Song < ActiveRecord::Base
     midi_song.tracks << melody
     midi_song.tracks << bass
     eighth_note_length = midi_song.note_to_delta('eighth')
+
+    melody.events << MIDI::NoteOn.new(0, 0, 127, 0)
+    melody.events << MIDI::NoteOff.new(0, 0, 127, eighth_note_length)
+    bass.events << MIDI::NoteOn.new(0, 0, 127, 0)
+    bass.events << MIDI::NoteOff.new(0, 0, 127, eighth_note_length)
+
     offset = 0
 
     self.pattern.split('').each do |note|
