30c63b094aa5f64ffa7ab54a5ef01efa4752fe04
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-03-25 16:43:21 -0400

    Support multi-syllable words with LyricsFetcher

--- app/models/lyrics_fetcher.rb
+++ app/models/lyrics_fetcher.rb
@@ -4,11 +4,24 @@ class LyricsFetcher
       raise "No words in database"
     end
 
-    lyrics = []
-    one_syllable_words = Word.where(syllables: 1)
-    number_of_syllables.times do
-      lyrics << one_syllable_words.sample.value
+    self.pick_lyrics_recursive(number_of_syllables)
+  end
+
+  private
+
+  def self.pick_lyrics_recursive(syllables)
+    if syllables == 0
+      []
+    else
+      this_word = Word.where("syllables <= ?", syllables).to_a.sample
+
+      if this_word
+        this_word = Word.where("syllables <= ?", syllables).to_a.sample
+        remaining_syllables = syllables - this_word.syllables
+        [this_word.value] + self.pick_lyrics_recursive(remaining_syllables)
+      else # no words found
+        raise "Couldn't create a lyric with the requested syllable count"
+      end
     end
-    lyrics
   end
 end
--- spec/models/lyrics_fetcher_spec.rb
+++ spec/models/lyrics_fetcher_spec.rb
@@ -2,9 +2,23 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe '#pick_lyrics' do
-  it 'picks random lyrics to fit the given number of syllables' do
-    create(:word, value: 'test', syllables: 1)
-    lyrics = LyricsFetcher.pick_lyrics(2)
-    expect(lyrics).to eq ['test', 'test']
+  context 'with one syllable words' do
+    it 'picks random lyrics to fit the given number of syllables' do
+      create(:word, value: 'test', syllables: 1)
+      lyrics = LyricsFetcher.pick_lyrics(2)
+      expect(lyrics).to eq ['test', 'test']
+    end
+  end
+
+  context 'with multi-syllable words' do
+    it 'only picks words that fit the given number of syllables' do
+      create(:word, value: 'syllable', syllables: 3)
+      create(:word, value: 'testing', syllables: 2)
+      lyrics = LyricsFetcher.pick_lyrics(2)
+      expect(lyrics).to eq ['testing']
+
+      lyrics = LyricsFetcher.pick_lyrics(5)
+      expect(lyrics).to include 'testing'
+    end
   end
 end
