3fa4f9547b564443372cffcb12378b5e13ab8c98
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-07-05 23:54:22 -0400

    Fix another typo

--- lib/wikihow_rhymes.rb
+++ lib/wikihow_rhymes.rb
@@ -13,7 +13,7 @@ class WikihowRhymes
 
   def self.client
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['WIKIHOW_RYHMES_SECRET_KEY']
+      config.consumer_key        = ENV['WIKIHOW_RHYMES_SECRET_KEY']
       config.consumer_secret     = ENV['WIKIHOW_RHYMES_CONSUMER_SECRET']
       config.access_token        = ENV['WIKIHOW_RHYMES_ACCESS_TOKEN']
       config.access_token_secret = ENV['WIKIHOW_RHYMES_ACCESS_TOKEN_SECRET']
