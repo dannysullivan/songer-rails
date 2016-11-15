7e91facea9ef58da4a0c94eada69dc94047d5a17
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-11-15 17:43:35 -0500

    Remove deprecated code related to sources

--- spec/controllers/couplets/sources_controller_spec.rb
+++ spec/controllers/couplets/sources_controller_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe Couplets::SourcesController do
   describe '#create' do
     it 'creates a new Couplets::Source' do
-      expect(Couplets::Source).to receive(:new).with(text: 'here is some text')
+      source = double(:source, syllable_groups: {})
+      expect(Couplets::Source).to receive(:new).with(text: 'here is some text').and_return(source)
       post :create, { couplets_source: { text: 'here is some text' } }
     end
   end
